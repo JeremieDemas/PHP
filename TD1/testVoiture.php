9e1cac4cbc8e02ce18ebc189041895781afecced
Jérémie Demas <jeremie.demas@gmail.com>
2018-11-23 14:06:23 +0100

    Exercice 6 terminé

---
+++ TD1/testVoiture.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    	<meta charset="utf-8" />
+        <title> Insérer le titrer ici </title>
+    </head>
+   
+    <body>
+    	<?php
+
+    		require_once('Voiture.php');
+    		$voiture1=new Voiture("Azerty","Uiop","AZE123");
+    		$voiture1->afficher();
+
+    	?>
+    </body>
+</html>
