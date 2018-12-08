09167ae36f3f86a22e8f2a6972e19f31fe1ac7f9
Jérémie Demas <jeremie.demas@gmail.com>
2018-12-08 19:55:36 +0100

    Exercice 8 du TD8 terminé

--- TD8/index.php
+++ TD8/index.php
@@ -3,5 +3,7 @@
 session_start();
 require_once('/var/www/html/PHP/TD8/lib/File.php');
 require_once File::build_path(array("controller","routeur.php"));
+require_once (File::build_path(array("lib", "Session.php")));
+
 
 ?>
--- TD8/view/utilisateur/detail.php
+++ TD8/view/utilisateur/detail.php
@@ -1,9 +1,13 @@
         <?php
 
+        require_once (File::build_path(array("lib", "Session.php")));
+
         foreach ($u as $value) {
             echo "<p> Utilisateur ".htmlspecialchars($value->get("prenom"))." ".htmlspecialchars($value->get("nom"))." de login ".htmlspecialchars($value->get("login")).". </p>";
-            echo "Pour supprimer cet utilisateur cliquez "."<a href=?action=delete&controller=utilisateur&login=".rawurlencode($value->get("login")).">ici.</a></p>";
-            echo "Pour mettre à jour cet utilisateur cliquez "."<a href=?action=update&controller=utilisateur&login=".rawurlencode($value->get("login")).">ici</a></p>";
+            if(Session::is_user($value->get("login"))) {
+	            echo "Pour supprimer cet utilisateur cliquez "."<a href=?action=delete&controller=utilisateur&login=".rawurlencode($value->get("login")).">ici.</a></p>";
+	            echo "Pour mettre à jour cet utilisateur cliquez "."<a href=?action=update&controller=utilisateur&login=".rawurlencode($value->get("login")).">ici</a></p>";
+	        }
         }
         
         ?>
